6944f631e95bd8ad3530d4f6db4d4526dd4fd0ef
RoyerMohamed <80180746+RoyerMohamed@users.noreply.github.com>
2023-07-07 10:11:22 +0200

    definition des route api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Opening_hourController;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +24,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('/shop' , ShopController::class)
+Route::apiResource('/shop' , ShopController::class);
+Route::apiResource('/product' , ShopController::class);
+Route::apiResource('/category' , ShopController::class);
+Route::apiResource('/image' , ShopController::class);
+Route::apiResource('/review' , ReviewController::class);
+Route::apiResource('/recipe' , RecipeController::class);
+Route::apiResource('/opening_hour' , Opening_hourController::class);
+Route::apiResource('/user' , UserController::class);
